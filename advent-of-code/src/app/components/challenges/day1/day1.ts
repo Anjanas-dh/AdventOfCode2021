d2bff098a7d459c62936219daf90bed57618f9d9
Anja van Hagen <anja.vanhagen@teamrockstars.nl>
2021-12-02 23:28:21 +0100

    Day 1 second challenge - not refactored

--- advent-of-code/src/app/components/challenges/day1/day1.ts
+++ advent-of-code/src/app/components/challenges/day1/day1.ts
@@ -13,17 +13,92 @@ export class Day1Component implements OnInit {
   ngOnInit() {
     this.challengeInput = Day1Challenge1InputData.init();
     this.challengeOne();
+    this.challengeTwo();
   }
 
   challengeOne() {
+    let totalIncrements: number = 0;
     for (let index = 0; index < this.challengeInput.length; index++) {
       if (
         index !== 0 &&
         this.challengeInput[index] > this.challengeInput[index - 1]
       ) {
-        this.answerChallengeOne++;
+        totalIncrements++;
       }
     }
-    return this.answerChallengeOne;
+    this.answerChallengeOne = totalIncrements;
+  }
+
+  challengeTwo() {
+    let totalIncrements: number = 0;
+    const split = 3;
+    const skip = 4;
+    let groupedATotal: number[] = [];
+    let groupedBTotal: number[] = [];
+    let groupedCTotal: number[] = [];
+    let groupedDTotal: number[] = [];
+    for (let i = 0, j = this.challengeInput.length; i < j; i += skip) {
+      groupedATotal.push(
+        this.challengeInput
+          .slice(i, i + split)
+          .reduce((partial_sum, a) => partial_sum + a, 0)
+      );
+    }
+    for (let i = 1, j = this.challengeInput.length; i < j; i += skip) {
+      groupedBTotal.push(
+        this.challengeInput
+          .slice(i, i + split)
+          .reduce((partial_sum, a) => partial_sum + a, 0)
+      );
+    }
+    for (let i = 2, j = this.challengeInput.length; i < j; i += skip) {
+      groupedCTotal.push(
+        this.challengeInput
+          .slice(i, i + split)
+          .reduce((partial_sum, a) => partial_sum + a, 0)
+      );
+    }
+    for (let i = 3, j = this.challengeInput.length; i < j; i += skip) {
+      groupedDTotal.push(
+        this.challengeInput
+          .slice(i, i + split)
+          .reduce((partial_sum, a) => partial_sum + a, 0)
+      );
+    }
+
+    for (let index = 0; index < groupedATotal.length; index++) {
+      if (
+        groupedBTotal[index] !== undefined &&
+        groupedATotal[index] !== undefined &&
+        groupedBTotal[index] > groupedATotal[index]
+      ) {
+        totalIncrements++;
+      }
+
+      if (
+        groupedCTotal[index] !== undefined &&
+        groupedBTotal[index] !== undefined &&
+        groupedCTotal[index] > groupedBTotal[index]
+      ) {
+        totalIncrements++;
+      }
+
+      if (
+        groupedDTotal[index] !== undefined &&
+        groupedCTotal[index] !== undefined &&
+        groupedDTotal[index] > groupedCTotal[index]
+      ) {
+        totalIncrements++;
+      }
+
+      if (
+        groupedATotal[index + 1] !== undefined &&
+        groupedDTotal[index] !== undefined &&
+        groupedATotal[index + 1] > groupedDTotal[index]
+      ) {
+        totalIncrements++;
+      }
+    }
+    this.answerChallengeTwo = totalIncrements;
   }
 }
