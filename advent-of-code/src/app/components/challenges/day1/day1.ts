07561c6046b92c604c681ea35ff1ce1db1fa1e8e
Anja van Hagen <anja.vanhagen@teamrockstars.nl>
2021-12-02 15:04:06 +0100

    Add day1challenge1 input data

--- advent-of-code/src/app/components/challenges/day1/day1.ts
+++ advent-of-code/src/app/components/challenges/day1/day1.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Day1Challenge1InputData } from './day1-challenge1-input.data';
 
 @Component({
   selector: 'app-day1',
   templateUrl: './day1.html',
 })
-export class Day1Component {}
+export class Day1Component {
+  input: number[] = Day1Challenge1InputData.init();
+}
