338b84f5c77db5fca8b96501948af826b2236200
Anja van Hagen <anja.vanhagen@teamrockstars.nl>
2021-12-02 23:57:20 +0100

    Refactor challenge two code

--- advent-of-code/src/app/components/challenges/day1/day1.ts
+++ advent-of-code/src/app/components/challenges/day1/day1.ts
@@ -30,75 +30,50 @@ export class Day1Component implements OnInit {
   }
 
   challengeTwo() {
-    let totalIncrements: number = 0;
+    this.answerChallengeTwo = this.calculateIncrements(
+      this.groupArrayByChunks()
+    );
+  }
+
+  private groupArrayByChunks() {
     const split = 3;
     const skip = 4;
-    let groupedATotal: number[] = [];
-    let groupedBTotal: number[] = [];
-    let groupedCTotal: number[] = [];
-    let groupedDTotal: number[] = [];
-    for (let i = 0, j = this.challengeInput.length; i < j; i += skip) {
-      groupedATotal.push(
-        this.challengeInput
-          .slice(i, i + split)
-          .reduce((partial_sum, a) => partial_sum + a, 0)
-      );
-    }
-    for (let i = 1, j = this.challengeInput.length; i < j; i += skip) {
-      groupedBTotal.push(
-        this.challengeInput
-          .slice(i, i + split)
-          .reduce((partial_sum, a) => partial_sum + a, 0)
-      );
-    }
-    for (let i = 2, j = this.challengeInput.length; i < j; i += skip) {
-      groupedCTotal.push(
-        this.challengeInput
-          .slice(i, i + split)
-          .reduce((partial_sum, a) => partial_sum + a, 0)
-      );
-    }
-    for (let i = 3, j = this.challengeInput.length; i < j; i += skip) {
-      groupedDTotal.push(
-        this.challengeInput
-          .slice(i, i + split)
-          .reduce((partial_sum, a) => partial_sum + a, 0)
-      );
-    }
-
-    for (let index = 0; index < groupedATotal.length; index++) {
-      if (
-        groupedBTotal[index] !== undefined &&
-        groupedATotal[index] !== undefined &&
-        groupedBTotal[index] > groupedATotal[index]
-      ) {
-        totalIncrements++;
-      }
-
-      if (
-        groupedCTotal[index] !== undefined &&
-        groupedBTotal[index] !== undefined &&
-        groupedCTotal[index] > groupedBTotal[index]
-      ) {
-        totalIncrements++;
-      }
-
-      if (
-        groupedDTotal[index] !== undefined &&
-        groupedCTotal[index] !== undefined &&
-        groupedDTotal[index] > groupedCTotal[index]
-      ) {
-        totalIncrements++;
-      }
-
-      if (
-        groupedATotal[index + 1] !== undefined &&
-        groupedDTotal[index] !== undefined &&
-        groupedATotal[index + 1] > groupedDTotal[index]
-      ) {
-        totalIncrements++;
+    let groupAll: { group: string; totals: number[] }[] = [
+      { group: 'A', totals: [] },
+      { group: 'B', totals: [] },
+      { group: 'C', totals: [] },
+      { group: 'D', totals: [] },
+    ];
+    for (let index = 0; index < 4; index++) {
+      for (let i = index, j = this.challengeInput.length; i < j; i += skip) {
+        groupAll[index].totals.push(
+          this.challengeInput
+            .slice(i, i + split)
+            .reduce((partial_sum, a) => partial_sum + a, 0)
+        );
       }
     }
-    this.answerChallengeTwo = totalIncrements;
+    return groupAll;
+  }
+
+  private calculateIncrements(groupAll: { group: string; totals: number[] }[]) {
+    let totalIncrements: number = 0;
+
+    for (let index = 0; index < groupAll[0].totals.length; index++) {
+      for (let i = 0; i < groupAll.length; i++) {
+        if (groupAll[i].totals[index] === undefined) {
+          break;
+        }
+        const groupIndex = i === groupAll.length - 1 ? 0 : i + 1;
+        const totalsIndex = i === groupAll.length - 1 ? index + 1 : index;
+        if (
+          groupAll[groupIndex].totals[totalsIndex] !== undefined &&
+          groupAll[groupIndex].totals[totalsIndex] > groupAll[i].totals[index]
+        ) {
+          totalIncrements++;
+        }
+      }
+    }
+    return totalIncrements;
   }
 }
