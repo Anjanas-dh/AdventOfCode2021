93fd2184bbdbc23e787885578d172cead31e4893
Anja van Hagen <anja.vanhagen@teamrockstars.nl>
2021-12-02 15:52:52 +0100

    Add snow to days page

--- advent-of-code/src/app/components/days/days.ts
+++ advent-of-code/src/app/components/days/days.ts
@@ -6,5 +6,12 @@ import { DaysData } from 'src/app/data/days.data';
   templateUrl: './days.html',
 })
 export class DaysComponent {
+  snowFlakes: number[] = new Array(
+    Number(
+      getComputedStyle(document.documentElement).getPropertyValue(
+        '--snow-count'
+      )
+    )
+  );
   daysCount: { dayNmbr: number; disabled: boolean }[] = DaysData.init();
 }
