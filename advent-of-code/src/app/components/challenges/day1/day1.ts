7c9367ea291791cd26846f480f75ba54eb756039
Anja van Hagen <anja.vanhagen@teamrockstars.nl>
2021-12-02 23:21:03 +0100

    Day 1 first challenge

--- advent-of-code/src/app/components/challenges/day1/day1.ts
+++ advent-of-code/src/app/components/challenges/day1/day1.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Day1Challenge1InputData } from './day1-challenge1-input.data';
 
 @Component({
   selector: 'app-day1',
   templateUrl: './day1.html',
 })
-export class Day1Component {
-  input: number[] = Day1Challenge1InputData.init();
+export class Day1Component implements OnInit {
+  challengeInput: number[] = [];
+  answerChallengeOne: number = 0;
+  answerChallengeTwo: number = 0;
+
+  ngOnInit() {
+    this.challengeInput = Day1Challenge1InputData.init();
+    this.challengeOne();
+  }
+
+  challengeOne() {
+    for (let index = 0; index < this.challengeInput.length; index++) {
+      if (
+        index !== 0 &&
+        this.challengeInput[index] > this.challengeInput[index - 1]
+      ) {
+        this.answerChallengeOne++;
+      }
+    }
+    return this.answerChallengeOne;
+  }
 }
